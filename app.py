799e6b53279465598143a7708c76b64b4c882de9
Danielle Meer <55391597+TIGERTIDES@users.noreply.github.com>
2024-09-19 21:52:09 +0800

    DLSU

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Lia Santos' News Articles", layout="centered")
 
-dataframed = pd.read_csv("https://raw.githubusercontent.com/ALGOREX-PH/AI_Republic_Isko_Reyes_UP/main/Dataset/Articles_AI.csv")
+dataframed = pd.read_csv("https://raw.githubusercontent.com/ALGOREX-PH/AI_Republic_Lia_Santos_DLSU/main/Dataset/Articles_AI.csv")
 
 if 'current_page' not in st.session_state:
     st.session_state.current_page = 'Home'
